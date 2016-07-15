63c2631d274866a98c2cf4c116807bae2332910a
Larry Hui <larry@larryhui.com>
2016-07-15 19:37:00 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # USPS-Tracking-Python
-Track your packages with the USPS Track Request API using Python
+Track your packages with the USPS Track Request API using Python 3
+
+IMPORTANT! You must provide your API key (sign up at https://www.usps.com/business/web-tools-apis/welcome.htm) in Line 13 before use!
+
+## Example
+
+```
+user@system:~$ python3 tracking.py ABC1234567890 DEF1234567890 GHI1234567890
+Your item arrived at our HURON, SD 57399 origin facility on July 12, 2016 at 9:16 pm. The item is currently in transit to the destination.
+Your item was accepted at 2:44 pm on July 14, 2016 in JAPAN.
+Your item arrived at our CHICAGO METRO origin facility on July 13, 2016 at 9:17 pm. The item is currently in transit to the destination.
+```
+
+## Usage
+
+You can track a single/multiple shipment as follows:
+
+```
+python3 tracking.py
+python3 tracking.py ABC1234567890
+python3 tracking.py ABC1234567890 DEF1234567890 GHI1234567890
+```
+
+If you run the program without a tracking number, it will prompt you for a single tracking number. You should use arguments for multiple package tracking.
+
+This program was tested on Python 3.4.2 running on Debian 8.4 and may not be compatible with previous releases.
---
+++ tracking.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# USPS API Tracking
+# Tested on Python 3.4.2 running on Debian 8.4
+# https://github.com/LiterallyLarry/USPS-Tracking-Python
+#
+# You must provide your API key in Line 13 before running this program! You can sign up for an API key here: https://www.usps.com/business/web-tools-apis/welcome.htm
+
+from urllib import request, parse
+from sys import argv
+from xml.etree import ElementTree
+
+def usps_track(numbers_list):
+    api_key = "YOUR_API_KEY_HERE";
+    usps_url = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2";
+    xml = "<TrackRequest USERID=\"%s\">" % api_key;
+    for track_id in numbers_list:
+        xml += "<TrackID ID=\"%s\"></TrackID>" % track_id;
+    xml += "</TrackRequest>";
+    target = "%s&%s" % (usps_url, parse.urlencode({ "XML" : xml }));
+    request_obj = request.urlopen(target);
+    result = request_obj.read();
+    request_obj.close();
+    return result;
+
+if __name__ == "__main__":
+    if len(argv) > 1: # System arguments support multiple tracking numbers
+        track_ids = argv[1:];
+    else:
+        track_id = input('Enter a tracking number: '); # User input prompt supports only a single number
+        if len(track_id) < 1:
+            exit(0);
+        track_ids = [ track_id ];
+    track_xml = usps_track(track_ids);
+    track_result = ElementTree.ElementTree(ElementTree.fromstring(track_xml));
+    for result in track_result.findall('Description'):
+        print(result.text);
+    for result in track_result.findall('.//TrackSummary'):
+        print(result.text);
