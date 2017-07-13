671b5ca198badf62101aeefc1ca6247dbcd8c0cf
Larry Hui <larryhui@larryhui.com>
2017-07-12 23:05:40 -0400

    Major Code Rewrite
    -Show extended tracking information
    -Store API key in separate configuration file
    -Change output formatting with program arguments
    -Allow silent execution with -m argument
    -Show only latest information with -n argument
    -Display tracking number in output with -s argument

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 # USPS-Tracking-Python
 Track your packages with the USPS Track Request API using Python 3
 
-IMPORTANT! You must provide your API key (sign up at https://www.usps.com/business/web-tools-apis/welcome.htm) in Line 13 before use!
-
-## Example
-
-```
-user@system:~$ python3 tracking.py ABC1234567890 DEF1234567890 GHI1234567890
-Your item arrived at our HURON, SD 57399 origin facility on July 12, 2016 at 9:16 pm. The item is currently in transit to the destination.
-Your item was accepted at 2:44 pm on July 14, 2016 in JAPAN.
-Your item arrived at our CHICAGO METRO origin facility on July 13, 2016 at 9:17 pm. The item is currently in transit to the destination.
-```
+IMPORTANT! You must provide your API key in config.json before use! Sign up at: https://www.usps.com/business/web-tools-apis/welcome.htm
 
 ## Usage
 
-You can track single/multiple shipment(s) as follows:
+You can track single/multiple shipment(s) by executing the program as follows:
 
 ```
 python3 tracking.py
@@ -22,6 +13,76 @@ python3 tracking.py ABC1234567890
 python3 tracking.py ABC1234567890 DEF1234567890 GHI1234567890
 ```
 
-If you run the program without a tracking number, it will prompt you for a single tracking number. You should use arguments for multiple package tracking.
+If you run the program without a tracking number, it will prompt you for a tracking number (you may input multiple tracking numbers by separating them with spaces).
 
-This program was tested on Python 3.4.2 running on Debian 8.4 and may not be compatible with previous releases.
+## Example
+
+```
+user@system:~$ python3 tracking.py
+Enter tracking numbers separated by spaces: ABC1234567890 DEF1234567890 GHI1234567890
+OK!
+
+Package #1:
+ Your item arrived at the PHILADELPHIA, PA 19101 post office at 11:02 am on June 19, 2017 and is ready for pickup.
+  ├ Arrived at Unit, June 19, 2017, 10:33 am, PHILADELPHIA, PA 19104
+  ├ Departed USPS Facility, June 17, 2017, 2:40 pm, PHILADELPHIA, PA 19116
+  ├ Arrived at USPS Facility, June 17, 2017, 2:22 pm, PHILADELPHIA, PA 19116
+  ├ Processed Through Facility, June 15, 2017, 1:29 am, ISC NEW YORK NY(USPS)
+  ├ Origin Post is Preparing Shipment
+  ├ Processed Through Facility, June 10, 2017, 6:00 am, TOKYO INT V BAG 2, JAPAN
+  └ Acceptance, June 6, 2017, 1:26 pm, JAPAN
+
+Package #2:
+ Your item was delivered at 6:14 pm on July 6, 2017 in PHILADELPHIA, PA 19104.
+  ├ Sorting Complete, July 6, 2017, 10:29 am, PHILADELPHIA, PA 19101
+  ├ Available for Pickup, July 6, 2017, 8:29 am, PHILADELPHIA, PA 19101
+  ├ Arrived at Post Office, July 6, 2017, 8:05 am, PHILADELPHIA, PA 19104
+  ├ Arrived at USPS Destination Facility, July 6, 2017, 2:00 am, PHILADELPHIA, PA 19176
+  ├ Processed Through Facility, July 5, 2017, 6:41 pm, ISC NEW YORK NY(USPS)
+  ├ Origin Post is Preparing Shipment
+  ├ Processed Through Facility, July 5, 2017, 6:20 am, TOKYO INT CONTAINER 1, JAPAN
+  ├ Processed Through Facility, July 4, 2017, 8:01 pm, TOKYO INT, JAPAN
+  └ Acceptance, July 4, 2017, 4:00 pm, JAPAN
+
+Package #3:
+ The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later.
+```
+
+You can use arguments to change the output format, like so:
+
+```
+user@system:~$ python3 tracking.py -h
+usage: tracking.py [-h] [-s] [-n] [-m] [TRACKING_NUMBER [TRACKING_NUMBER ...]]
+
+Tracks USPS numbers via Python.
+
+positional arguments:
+  TRACKING_NUMBER  a tracking number
+
+optional arguments:
+  -h, --help       show this help message and exit
+  -s               Show tracking number in output
+  -n               Hide extended tracking information
+  -m               Repress UI
+```
+
+```
+user@system:~$ python3 tracking.py ABC1234567890 -m
+Your item was delivered at 6:14 pm on July 6, 2017 in PHILADELPHIA, PA 19104.
+  ├ Sorting Complete, July 6, 2017, 10:29 am, PHILADELPHIA, PA 19101
+  ├ Available for Pickup, July 6, 2017, 8:29 am, PHILADELPHIA, PA 19101
+  ├ Arrived at Post Office, July 6, 2017, 8:05 am, PHILADELPHIA, PA 19104
+  ├ Arrived at USPS Destination Facility, July 6, 2017, 2:00 am, PHILADELPHIA, PA 19176
+  ├ Processed Through Facility, July 5, 2017, 6:41 pm, ISC NEW YORK NY(USPS)
+  ├ Origin Post is Preparing Shipment
+  ├ Processed Through Facility, July 5, 2017, 6:20 am, TOKYO INT CONTAINER 1, JAPAN
+  ├ Processed Through Facility, July 4, 2017, 8:01 pm, TOKYO INT, JAPAN
+  └ Acceptance, July 4, 2017, 4:00 pm, JAPAN
+```
+
+```
+user@system:~$ python3 tracking.py ABC1234567890 -mn
+Your item was delivered at 6:14 pm on July 6, 2017 in PHILADELPHIA, PA 19104.
+```
+
+This program was tested on Python 3.4.2 running on Debian 8.7 and may not be compatible with previous releases.
--- tracking.py
+++ tracking.py
@@ -1,38 +1,95 @@
 #!/usr/bin/env python3
 # USPS API Tracking
-# Tested on Python 3.4.2 running on Debian 8.4
+# Tested on Python 3.4.2 running on Debian 8.7
 # https://github.com/LiterallyLarry/USPS-Tracking-Python
 #
-# You must provide your API key in Line 13 before running this program! You can sign up for an API key here: https://www.usps.com/business/web-tools-apis/welcome.htm
+# You must provide your API key in config.json as  before running this program! You can sign up for an API key here: https://www.usps.com/business/web-tools-apis/welcome.htm
 
 from urllib import request, parse
 from sys import argv
 from xml.etree import ElementTree
+import argparse, json, sys, os
+
+USPS_API_URL = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2";
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(path, "config.json")) as config_file:
+    config = json.load(config_file);
+    api_key = config.get("api_key");
+
+if not api_key:
+    sys.exit("Error: Could not find USPS API key in config.json!");
+
+parser = argparse.ArgumentParser(description='Tracks USPS numbers via Python.');
+
+parser.add_argument('tracking_numbers', metavar='TRACKING_NUMBER', type=str, nargs='*',
+                    help='a tracking number');
+parser.add_argument('-s', action='store_true', default=False,
+                    dest='show_tracking_number',
+                    help='Show tracking number in output');
+parser.add_argument('-n', action='store_false', default=True,
+                    dest='show_tracking_extended',
+                    help='Hide extended tracking information');
+parser.add_argument('-m', action='store_true', default=False,
+                    dest='show_minimal',
+                    help='Repress UI');
 
 def usps_track(numbers_list):
-    api_key = "YOUR_API_KEY_HERE";
-    usps_url = "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2";
     xml = "<TrackRequest USERID=\"%s\">" % api_key;
     for track_id in numbers_list:
         xml += "<TrackID ID=\"%s\"></TrackID>" % track_id;
     xml += "</TrackRequest>";
-    target = "%s&%s" % (usps_url, parse.urlencode({ "XML" : xml }));
+    target = "%s&%s" % (USPS_API_URL, parse.urlencode({ "XML" : xml }));
     request_obj = request.urlopen(target);
     result = request_obj.read();
     request_obj.close();
     return result;
 
 if __name__ == "__main__":
-    if len(argv) > 1: # System arguments support multiple tracking numbers
-        track_ids = argv[1:];
+    args = parser.parse_args();
+    if args.tracking_numbers: # Arguments support multiple tracking numbers
+        track_ids = args.tracking_numbers;
+        #track_ids = argv[1:];
     else:
-        track_id = input('Enter a tracking number: '); # User input prompt supports only a single number
+        #track_id = input(); # User input supports only a single number
+        track_id = input('Enter tracking numbers separated by spaces: '); # User input supports multiple tracking numbers split with spaces
         if len(track_id) < 1:
             exit(0);
-        track_ids = [ track_id ];
+        track_ids = track_id.split(' ');
+        #track_ids = [ track_id ];
+    real = []
+    for id in track_ids:
+        if id[0] != '#':
+            real.append(id);
+    track_ids = real
     track_xml = usps_track(track_ids);
+#    print(track_xml);
     track_result = ElementTree.ElementTree(ElementTree.fromstring(track_xml));
-    for result in track_result.findall('Description'): # Print error messages
+    if not args.show_minimal:
+        print('OK!');
+    for result in track_result.findall('Description'):
         print(result.text);
-    for result in track_result.findall('.//TrackSummary'): # Print tracking messages
-        print(result.text);
+#    for result in track_result.findall('.//TrackSummary'):
+#        print(result.text);
+    for number, result in enumerate(track_result.findall('.//TrackInfo')):
+        if args.show_tracking_number:
+            track_num = ' (%s)' % track_ids[number];
+        else:
+            track_num = ''
+        summary = result.find('TrackSummary');
+        if summary is None:
+            print('Error in XML!');
+            print(track_xml);
+        else:
+            if args.show_minimal:
+                print('%s' % summary.text);
+            else:
+                print('\nPackage #%d%s:\n %s' % (number+1,track_num,summary.text));
+            if args.show_tracking_extended:
+                details = result.findall('TrackDetail');
+                for number_2, detailed_result in enumerate(details):
+                    if number_2+1 == len(details):
+                        print('  └ %s' % detailed_result.text);
+                    else:
+                        print('  ├ %s' % detailed_result.text);
